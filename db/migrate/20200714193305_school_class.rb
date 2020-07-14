235491894875ba7aad2c6e3fa35fcacb7584753b
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 19:35:50 +0000

    Automatically backed up by Learn

--- db/migrate/20200714193305_school_class.rb
+++ db/migrate/20200714193305_school_class.rb
@@ -1,5 +1,7 @@
 class SchoolClass < ActiveRecord::Migration[5.0]
   def change
-    create_table :school_class do |t|
+    create_table :school_classes do |t|
+      t.string :title 
+      t.integer :room_number
   end
 end
