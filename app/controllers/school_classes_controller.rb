b5e7990e116ab59ddb7973e50b347fa4430d2d6f
Parker Williamson <parker.williamson@gmail.com>
2020-07-14 19:51:21 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,21 @@
 class SchoolClassesController < ApplicationController 
+  def show
+    
+  end
   
+  def new 
+    
+  end 
+  
+  def create 
+    
+  end
+  
+  def edit 
+    
+  end
+  
+  def update 
+    
+  end
 end
